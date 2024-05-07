d49febc10b8579f1c8d91702a0e3243d35edf503
leonardoXLT <148663990+leonardoXLT@users.noreply.github.com>
2024-05-07 19:50:12 +1000

    feat: render X elements based on state number

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+
+class App extends React.Component {
+  constructor(){
+    super();
+
+    this.state = 
+    { 
+      numberOfPokemonToShow: 6
+    }
+
+    this.increaseNumber = this.increaseNumber.bind(this);
+    this.decreaseNumber = this.decreaseNumber.bind(this);
+  }
+  
+  exampleFunction(){
+    console.log("Example function was run!");
+  }
+
+  increaseNumber(){
+    this.setState({numberOfPokemonToShow: this.state.numberOfPokemonToShow + 1});
+  }
+
+  decreaseNumber(){
+    this.setState({numberOfPokemonToShow: this.state.numberOfPokemonToShow - 1});
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <h1>Number of Pokemon to show: {this.state.numberOfPokemonToShow}</h1>
+        <button onClick={this.increaseNumber} >
+          Increase number 
+        </button>
+        <button onClick={this.decreaseNumber}>
+          Decrease number
+        </button>
+
+        {/* Create an array of size {numberOfPokemonToShow}, give each element in the array a value of null */}
+        {/* For each item in the array, render a HTML element (eg. a H1) */}
+        {
+          Array(this.state.numberOfPokemonToShow)
+          .fill(null)
+          .map((element, index) => { 
+            return <h1>{index}</h1>
+          })
+        }
+
+      </div>
+    );
+  }
+}
+
+export default App;
